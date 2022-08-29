75b62a3cadf6db045844931ecb466fcce69e9899
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-08-29 11:49:23 -0300

    feat(app): function to create task element

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const criarItem = (tarefa) => {
+//função que ensina o Javascript a criar o elemento que queremos
+const criarItem = (tarefa, status='') => {
     const item = document.createElement('label');
     item .classList.add("to-do__item");
     item.innerHTML = `
-        <input type = "checkbox">
+        <input type = "checkbox" ${status}>
         <div>${tarefa}</div>
         <input type = "button" value = "X">
     `
