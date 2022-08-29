f1c1c653875f4db50a5b6b2adff26089b1475acf
Julia Santinho da Fonseca <juliafsantinho@gmail.com>
2022-08-29 16:22:03 -0300

    feat(app): Configuring data in LocalStorage

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 'use strict';
  
 // Criando o JSON para simular um banco de dados
-let bancoDeDados = [
-    {'tarefa':' Estudar', 'status': ''},
-    {'tarefa': ' Lavar Louça', 'status': 'checked'},
-    {'tarefa': ' Academia', 'status': ''},
+let banco = [
+//    {'tarefa':' Estudar', 'status': ''},
+//    {'tarefa': ' Lavar Louça', 'status': 'checked'},
+//    {'tarefa': ' Academia', 'status': ''},
 ];
+
+//Configurando os dados no LocalStorage
+const getBanco = () => JSON.parse(localStorage.getItem ('to-doList')) ?? [];      //Pega informação do meu banco
+const setBanco = (banco) => localStorage.setItem ('to-doList', JSON.stringify(banco));     //Atualiza informação do meu banco
 
 // Função que ensina o Javascript a criar o elemento que queremos
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
-    item .classList.add("to-do__item");
+    item .classList.add("todo__item");
     item.innerHTML = `
         <input type = "checkbox" ${status} data-indice = ${indice}>
         <div>${tarefa}</div>
-        <input type = "button" value = "X" data-indice = ${indice}>
+        <input type = "button" value ="X" data-indice = ${indice}>
     `
     document.getElementById('todoList').appendChild(item);
 }
@@ -28,7 +32,8 @@ const limparTarefas = () => {
 
 const atualizarTarefa = () => {
     limparTarefas();
-    bancoDeDados.forEach ((item, indice) => criarItem (item.tarefa, item.status, indice))     // forEach = percorre o array item a item
+    const banco = getBanco();
+    banco.forEach ((item, indice) => criarItem (item.tarefa, item.status, indice));    // forEach = percorre o array item a item
 }
 
 // Criando a função para inserir uma nova tarefa
@@ -36,7 +41,9 @@ const inserirItem = (evento) => {
     const tecla = evento.key;
     const texto = evento.target.value;
     if (tecla === 'Enter'){
-        bancoDeDados.push({'tarefa': texto, 'status': ''});
+        const banco = getBanco();
+        banco.push({'tarefa': texto, 'status': ''});
+        setBanco(banco);
         atualizarTarefa();
         evento.target.value = ''; // limpando a tarefa da caixa de texto ao atualizar a pagina
     }
@@ -44,13 +51,17 @@ const inserirItem = (evento) => {
 
 // Programando a ação de deletar tarefas
 const removerItem = (indice) => {
-    bancoDeDados.splice(indice, 1);
+    const banco = getBanco();
+    banco.splice(indice, 1);
+    setBanco(banco);
     atualizarTarefa();
 }
 
 //Programando a ação de atualizar o status da tarefa
 const atualizarItem = (indice) => {
-    bancoDeDados[indice].status = bancoDeDados[indice].status === '' ? 'checked' : '';
+    const banco = getBanco();
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
     atualizarTarefa();
 }
 
@@ -58,10 +69,11 @@ const atualizarItem = (indice) => {
 // Identificando as tarefas com índice
 const clickItem = (evento) => {
     const elemento = evento.target;
+    console.log(elemento.type);
     if(elemento.type === 'button') {
-        const indice = elemento.dataset.indice
+        const indice = elemento.dataset.indice;
         removerItem(indice);
-    } else if(elemento.type === 'checkbox') {
+    } else if (elemento.type === 'checkbox') {
         const indice = elemento.dataset.indice;
         atualizarItem(indice);
     }
